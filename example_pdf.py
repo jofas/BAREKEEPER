4b526c38ea9b7ebfc48ac8d8bb5ae55cebeeb3bf
jofas <jonas@fc-web.de>
2022-05-20 16:06:15 +0200

    OpenSans + work on tables

--- example_pdf.py
+++ example_pdf.py
@@ -8,9 +8,6 @@ Germany
 
 Tel.: +49 1245 678910
 E-Mail: test@company1.de
-
-
-
 """
 
 recipient="""\
@@ -18,17 +15,14 @@ Company 2
 Main Street 12b
 12345 Somewhere
 Germany
-
-
-
 """
 
-back_addr="Company 1 | Second Ave 2131 | 54321 Nowhere\n\n"
+back_addr="Company 1 | Second Ave 2131 | 54321 Nowhere"
 
 width = 210
 height = 297
 
-lh = 5
+lh = 4.6
 
 pdf = FPDF()
 
@@ -40,24 +34,56 @@ pdf.set_font("Helvetica", size=10)
 
 pdf.multi_cell(0, lh, align="R", txt=sender)
 
-# TODO: margin
+pdf.ln(lh)
 
 pdf.set_font("Helvetica", size=8)
 
-pdf.multi_cell(0, lh, align="L", txt=back_addr)
+pdf.multi_cell(0, lh, txt=back_addr)
 
+pdf.ln(lh)
 
 pdf.set_font("Helvetica", size=10)
 
-pdf.multi_cell(0, lh, align="L", txt=recipient)
+pdf.multi_cell(0, lh, txt=recipient)
 
-pdf.multi_cell(0, lh, align="R", txt="12. März 2021\n\n")
+pdf.ln(lh)
+
+pdf.multi_cell(0, lh, align="R", txt="12. März 2021")
+
+pdf.ln(lh)
 
 pdf.set_font("Helvetica", "B", size=16)
 
 pdf.multi_cell(0, lh, align="L", txt="Rechnung Nr. 1")
 
-# TODO: table (try 10%, 83% 7%)
+pdf.ln(lh)
+
+pdf.add_font("OpenSans", fname="fonts/open_sans/OpenSans-VariableFont_wdth,wght.ttf", uni=True)
+pdf.set_font("OpenSans", size=10)
+
+col_widths = [0.25, 0.6, 0.15]
+
+data = [
+    ["01.03.2021", "A work item", "12,99 €"],
+    ["02.03.2021 - 12.03.2021", "A second work item", "400,00 €"],
+    ["", "Zwischensummer ohne MwSt.", "412.99 €"],
+    ["", "19% MwSt.", "78.74 €"],
+    ["", "Gesamtbetrag", "491.46 €"],
+]
+
+for entry in data:
+    for i, c in enumerate(entry):
+        pdf.multi_cell(
+            (width - 40) * col_widths[i],
+            lh,
+            align="L",
+            txt=str(c),
+            border=1,
+            ln=3,
+            #new_x="RIGHT",
+            #new_y="TOP",
+        )
+    pdf.ln(lh)
 
 # TODO: footer
 
